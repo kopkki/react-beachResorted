3d5ef498ef1510f083f5b74b5fa9ab04b604aea8
kopki <kopki684@gmail.com>
2021-08-20 15:04:06 +0800

    key variable edit

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: "c0lf6hi2joph",
+  accessToken: "n4RJdbBFDP86EwP_-Xvt6hfbEL3itAKA_rsSTUTgcuI",
 });
